28e4883247aa976607c15e268250e9cf0e1e1b89
Ilya Anikeyenka <20prorok02@gmail.com>
2023-06-18 01:22:37 +0300

    Solution

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,24 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                list.add(entry.getKey());
+            }
+        }
+        list.sort(new Comparator<>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() != o2.length()) {
+                    return o2.length() - o1.length();
+                }
+                return o2.compareTo(o1);
+            }
+        });
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,21 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            Set<String> set = new HashSet<>();
+            if (map.containsKey(entry.getKey().length())) {
+                set = map.get(entry.getKey().length());
+            }
+            set.add(entry.getKey());
+            map.put(entry.getKey().length(), set);
+        }
+        return map;
     }
 }
